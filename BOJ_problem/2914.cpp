991bf38917472116c2474e9537f3ba5a02da73d9
백인걸 <big9401@gmail.com>
2021-04-25 23:37:19 +0900

    update 2914.cpp

---
+++ BOJ_problem/2914.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    int result = a * (b - 1) + 1;
+    
+    cout << result << endl;
+    return 0;
+}
