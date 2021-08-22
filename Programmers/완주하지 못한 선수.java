9b7684b96143ef3367c41e6516b8f57085a74304
geol2 <big9401@gmail.com>
2021-08-22 17:07:39 +0900

    HashMap : 완주하지 못한 선수

---
+++ Programmers/완주하지 못한 선수.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Programmers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] alphabet = { "A", "B", "C" ,"A"}; 
+		
+		HashMap<String, Integer> hm = new HashMap<>(); 
+		for(String key : alphabet) {
+			hm.put(key, hm.getOrDefault(key, 0) + 1); 
+			System.out.println(hm.getOrDefault(key, 0) + 1);
+		}
+		System.out.println("°á°ú : " + hm);
+	}
+
+}
