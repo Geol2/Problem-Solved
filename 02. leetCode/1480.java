e597d882e14b5d1d83a4ee3cb0ca9df23d1c2a6d
geol <big9401@gmail.com>
2025-02-12 09:16:53 +0900

    dx dy 기법 추가

--- 02. leetCode/1480.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] runningSum(int[] nums) {
-        for(int i = 0; i < nums.length - 1; i++) {
-            int num = nums[i] + nums[i+1];
-            nums[i+1] = num;
-        }
-        return nums;
-    }
-}
