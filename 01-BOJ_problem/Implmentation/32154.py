d02b05d6942dde37d4f8f7848a644b179420dc7a
IMG-BAEK\big94 <big9401@exsoft.co.kr>
2026-01-13 10:35:41 +0900

    add : 21598, 32154

---
+++ 01-BOJ_problem/Implmentation/32154.py
@@ -0,0 +1,46 @@
+score = {
+    1: {
+        'solved': 11,
+        'value': "A B C D E F G H J L M",
+    },
+    2: {
+        'solved': 9,
+        'value': "A C E F G H I L M",
+    },
+    3: {
+        'solved': 9,
+        'value': "A C E F G H I L M",
+    },
+    4: {
+        'solved': 9,
+        'value': "A B C E F G H L M",
+    },
+    5: {
+        'solved': 8,
+        'value': "A C E F G H L M",
+    },
+    6: {
+        'solved': 8,
+        'value': "A C E F G H L M",
+    },
+    7: {
+        'solved': 8,
+        'value': "A C E F G H L M",
+    },
+    8: {
+        'solved': 8,
+        'value': "A C E F G H L M",
+    },
+    9: {
+        'solved': 8,
+        'value': "A C E F G H L M",
+    },
+    10: {
+        'solved': 8,
+        'value': "A B C F G H L M",
+    },
+}
+
+n = int(input())
+print(score[n]['solved'])
+print(score[n]['value'])
