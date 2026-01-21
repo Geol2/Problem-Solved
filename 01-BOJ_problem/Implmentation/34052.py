13ee9877ce12e0dcf2bac71cc8fa8add257f4ea9
geol <big9401@gmail.com>
2026-01-21 18:11:37 +0900

    add : 32915.py, 34052.py

---
+++ 01-BOJ_problem/Implmentation/34052.py
@@ -0,0 +1,12 @@
+T1 = int(input())
+T2 = int(input())
+T3 = int(input())
+T4 = int(input())
+
+final_time = 300
+total = T1 + T2 + T3 + T4 + final_time
+
+if 1800 >= total:
+    print("Yes")
+else:
+    print("No")
