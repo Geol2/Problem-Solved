fe1b1368cfb7691dc5f5406e24d998dde270af9a
geol <big9401@gmail.com>
2026-03-11 13:33:43 +0900

    add : 23812.py

---
+++ 01-BOJ_problem/Implmentation/23812.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+for i in range(N):
+    print("@" * N + " " * N + " " * N + " " * N + "@" * N)
+
+for i in range(N):
+    print("@@@@@" * N)
+
+for i in range(N):
+    print("@" * N + " " * N + " " * N + " " * N + "@" * N)
+
+for i in range(N):
+    print("@@@@@" * N)
+
+for i in range(N):
+    print("@" * N + " " * N + " " * N + " " * N + "@" * N)
+
+    
