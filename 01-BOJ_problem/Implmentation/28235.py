0080f845fa33b7275bee315e9e95b2197523ad10
geol <big9401@gmail.com>
2026-03-18 18:11:55 +0900

    add : 28235.py

---
+++ 01-BOJ_problem/Implmentation/28235.py
@@ -0,0 +1,10 @@
+S = input()
+
+if S == "SONGDO":
+    print("HIGHSCHOOL")
+elif S == "CODE":
+    print("MASTER")
+elif S == "2023":
+    print("0611")
+elif S == "ALGORITHM":
+    print("CONTEST")
