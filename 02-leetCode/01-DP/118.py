04e30d464d405cb8789158e9201c94cf11e9e8ed
geol <big9401@gmail.com>
2026-05-07 23:43:21 +0900

    add : 118.py

---
+++ 02-leetCode/01-DP/118.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        t = []
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = t[i-1][j-1] + t[i-1][j]
+            t.append(row) 
+        
+        return t
+
+sol = Solution()
+arr = sol.generate(4)
+print(arr)
