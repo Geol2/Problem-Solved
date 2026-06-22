08c596e615cb0020d19e0d7484f61386c20a074b
geol <big9401@gmail.com>
2026-06-22 15:57:30 +0900

    add : 1668.py

---
+++ 02-leetCode/01-DP/1668.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxRepeating(self, sequence, word):
+        """
+        :type sequence: str
+        :type word: str
+        :rtype: int
+        """
+        k = 0
+        while word * (k + 1) in sequence:
+            k += 1
+        return k
+
+print(Solution().maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba"))
